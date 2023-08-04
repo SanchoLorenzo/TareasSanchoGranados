805ec5b492cb047ff0d90d9d4dd0243b0926a05c
SanchoLorenzo <lorenzo7sancho@gmail.com>
2023-08-03 22:37:46 -0600

    Subi un nuevo archivo con los errores de formato

---
+++ Tarea_1/extra.py
@@ -0,0 +1,10 @@
+def hola_mundo(nombre, edad, comida)  :
+
+
+
+
+    return ("Hola mi nombre es " + nombre + ", tengo "
+            + edad + " años " "y me gusta comer " + comida + ".")
+
+
+print(   hola_mundo("Kaleb", "19", "arroz"))
