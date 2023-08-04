248679141182090d3ca4560e4efd21914ff6e1a7
SanchoLorenzo <lorenzo7sancho@gmail.com>
2023-08-03 22:39:35 -0600

    Corregí los errores de formato

--- Tarea_1/extra.py
+++ Tarea_1/extra.py
@@ -1,10 +1,7 @@
-def hola_mundo(nombre, edad, comida)  :
-
-
-
+def hola_mundo(nombre, edad, comida):
 
     return ("Hola mi nombre es " + nombre + ", tengo "
             + edad + " años " "y me gusta comer " + comida + ".")
 
 
-print(   hola_mundo("Kaleb", "19", "arroz"))
+print(hola_mundo("Kaleb", "19", "arroz"))
